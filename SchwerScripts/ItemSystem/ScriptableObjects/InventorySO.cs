0f83f691bc0c084b4636e8cffcc168d5fa0a2398
Schwer <42115696+itsschwer@users.noreply.github.com>
2020-06-02 20:43:44 +1000

    Initial file upload.

---
+++ SchwerScripts/ItemSystem/ScriptableObjects/InventorySO.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Schwer.ItemSystem {
+    [CreateAssetMenu(menuName = "Scriptable Object/Inventory")]
+    public class InventorySO : ScriptableObject {
+        public Inventory value = new Inventory();
+    }
+}
